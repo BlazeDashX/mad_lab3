68b7de6ea0b9cc0977e891d3cff5a771e938ad31
BlazeDashX <refat00021@gmail.com>
2026-08-20 01:21:57 +0600

    feat: implement home navigation and article detail view using stack router

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native";
-
 import ArticleCard from "@/components/article-card";
 
 export default function HomeScreen() {
@@ -18,22 +16,3 @@ export default function HomeScreen() {
         />
     );
 }
-
-const styles = StyleSheet.create({
-    titleContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 8,
-    },
-    stepContainer: {
-        gap: 8,
-        marginBottom: 8,
-    },
-    reactLogo: {
-        height: 178,
-        width: 290,
-        bottom: 0,
-        left: 0,
-        position: "absolute",
-    },
-});
